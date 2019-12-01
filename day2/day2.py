0bc8afa0250ba47b1195c29635b8e95053964528
Levente Vig <vig.levente@codeyard.eu>
2019-12-01 06:21:45 +0100

    ✨Added empty day2

---
+++ day2/day2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+import os
+
+
+dirname = os.path.dirname(__file__)
+inputfile = os.path.join(dirname, 'input.txt')
+
+
+def part1():
+    with open(inputfile) as file:
+            for line in file:
+                print(line)
+
+
+def part2():
+    with open(inputfile) as file:
+            for line in file:
+                print(line)
+
+
+def main():
+    part1()
+    part2()
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
