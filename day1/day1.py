66fbf4f752d84f3b1a84cfea70a9b111d83c0343
Levente Vig <vig.levente@codeyard.eu>
2019-12-01 06:18:57 +0100

    ⭐️⭐️ Day1 completed

--- day1/day1.py
+++ day1/day1.py
@@ -11,20 +11,34 @@ inputfile = os.path.join(dirname, 'input.txt')
 
 def part1():
     with open(inputfile) as file:
-            for line in file:
-                print(line)
+        total = 0
+        for line in file:
+            mass = int(line)
+            total += int(mass / 3.0) - 2
+        print("Part 1: {total}".format(total=total))
+
+
+def getFuelFor(mass):
+    if mass <= 0:
+        return 0
+    fuel = int(mass / 3.0) - 2
+    if fuel <= 0:
+        return 0
+    return fuel + getFuelFor(fuel)
 
 
 def part2():
     with open(inputfile) as file:
-            for line in file:
-                print(line)
+        total = 0
+        for line in file:
+            mass = int(line)
+            total += getFuelFor(mass)
+        print("Part 2: {total}".format(total=total))
 
 
 def main():
     part1()
     part2()
-    print("hello")
 
 
 if __name__ == "__main__":
